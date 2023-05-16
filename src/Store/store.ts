0e17125718c4964bf2c3a6f23773c7fa44ce2d7f
Sakariye Awale <99921216+sak45@users.noreply.github.com>
2023-05-16 23:45:01 +0100

    added search to store

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,6 +1,3 @@
-import { create } from 'zustand'
-import { data } from "../lib/data.js";
-
 export interface Tweet {
   id: number;
   category: string;
@@ -15,9 +12,28 @@ export interface TweetState {
   removeTweet: (id: number) => void;
 }
 
+// Here is the interface for the search tweets
+export interface SearchState {
+  // this is the property that are declared to their type.
+  searchTweet: string, 
+  searchResult: Tweet[];
+  // these are the states that will be changing 
+  setSearchTweet: (search: string) => void,
+  removeSearchTweet: (id: number) => void, 
+}
+
 export const useTweetStore = create<TweetState>()((set) => ({
   tweets: data,
   addTweet: (tweet) => set((state) => ({tweets: [...state.tweets, tweet]})),
   removeTweet: (id) => set((state) => ({ tweets: state.tweets.filter(tweet => tweet.id !== id)}))
 }))
+
+//  create your own store searchStore 
+// future reference this is the state that will be updating e.g re-rendering
+export const useSearchState = create<SearchState>()((set) =>({
+  searchTweet: '',
+  setSearchTweet: (search) => set(({searchTweet: search})),
+  removeSearchTweet: () => set({searchTweet: ''}),
+  searchResult: []
+}) )
 
